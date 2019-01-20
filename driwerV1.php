966aba3295eaf08b2158dc70785a7ff182f16e67
MaksiWolf <36965057+MaksiWolf@users.noreply.github.com>
2019-01-20 23:42:20 +0300

    Update driwerV1.php

--- driwerV1.php
+++ driwerV1.php
@@ -188,12 +188,13 @@ function printResult($arr, $printHat)
 }
 function fetchData($url, $arr)
 {
+    global $admin, $adminPass; 	
     $ch = curl_init($url.'bot.php');
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_USERAGENT, "User-Agent: Mozilla/4.0");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_USERPWD, "admin:ПАРОЛЬ");
+	curl_setopt($ch, CURLOPT_USERPWD, $admin.":".$adminPass);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_TIMEOUT_MS, 60000);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($arr));
