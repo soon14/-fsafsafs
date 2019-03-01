919a67c68dcbdf8ee9a8a58d5f4920fe90ae14f9
MaksiWolf <36965057+MaksiWolf@users.noreply.github.com>
2019-03-01 14:29:34 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -41,7 +41,7 @@ if (!empty($_POST['login']) && !empty($_POST['password'])) { //если введ
 function shutDownFunction() { 
 	chdir(dirname(__FILE__));
     $error = error_get_last();
-    file_put_contents("test.log",implode(",",$error)."11\n", FILE_APPEND);
+    file_put_contents("test.log",implode(",",$error)."|11|\n", FILE_APPEND);
 }
 register_shutdown_function('shutDownFunction');
 $parseArr = $_POST;
@@ -65,28 +65,28 @@ function getFbInfo($handles, $fromMs, $toMs)
     $noPage2 = 'Содержание';
     $noPage3 = 'Facebook';
     //____________________________________________________________________________
-file_put_contents("test.log",implode(",",error_get_last())."1\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|1|\n", FILE_APPEND);
     $Arr = new LimitIterator(new ArrayIterator($parseArr), $shiftArray);
     //$testString = '';
     if (count($parseArr) - $shiftArray > 1) {
         foreach ($Arr as $key => $element) {
             $resultUrl = $urlGetFacebook . $element['loginUser'].'/about?section=contact-info';
             $driver->get($resultUrl);
-file_put_contents("test.log",implode(",",error_get_last())."2\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|2|\n", FILE_APPEND);
 			randomScroll(1500000, 1600000);
             $htmlFb = $driver->getPageSource();
             $lnfn = getLnFn($htmlFb);
-file_put_contents("test.log",implode(",",error_get_last())."3\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|3|\n", FILE_APPEND);
             $arrNameInstagram = explode(' ', $element['userName']);
-file_put_contents("test.log",implode(",",error_get_last())."4\n", FILE_APPEND);		
+file_put_contents("test.log",implode(",",error_get_last())."|4|\n", FILE_APPEND);		
             if (!Empty($lnfn['ln']) && stristr($lnfn['ln'], $noPage2, 0) !== false) {
                 $resultUrl = $urlGetFacebook . $element['loginUser'];
                 $driver->get($resultUrl);
-file_put_contents("test.log",implode(",",error_get_last())."5\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|5|\n", FILE_APPEND);
                 $htmlFb = $driver->getPageSource();
                 $lnfn = getLnFn($htmlFb);
             }
-file_put_contents("test.log",implode(",",error_get_last())."6\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|6|\n", FILE_APPEND);
             if (stristr($lnfn['ln'], $noPage, 0) === false &&
                 stristr($lnfn['ln'], $noPage2, 0) === false &&
                 stristr($lnfn['ln'], $noPage3, 0) === false){
@@ -94,7 +94,7 @@ file_put_contents("test.log",implode(",",error_get_last())."6\n", FILE_APPEND);
                 $ArrHtml = getArrHtml($htmlFb);
                 $digital = getOnlyDigital($ArrHtml, 9, 15);
                 $ArrName = getLnFn($htmlFb);
-file_put_contents("test.log",implode(",",error_get_last())."7\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|7|\n", FILE_APPEND);
 
                 $arrayResult[$shift]['url'] = $resultUrl;
                 $parseArr[$key]['facebookLink'] = $resultUrl;
@@ -106,14 +106,14 @@ file_put_contents("test.log",implode(",",error_get_last())."7\n", FILE_APPEND);
 		$arrayResult[$shift]['citynew'] = getCityNew($htmlFb);
 		$arrayResult[$shift]['cityold'] = getCityOld($htmlFb);
                 $shift++;
-file_put_contents("test.log",implode(",",error_get_last())."8\n", FILE_APPEND);		    
+file_put_contents("test.log",implode(",",error_get_last())."|8|\n", FILE_APPEND);		    
                 if (time() - $timeHis > $betweenWriting) {
-file_put_contents("test.log",implode(",",error_get_last())."81\n", FILE_APPEND);				
+file_put_contents("test.log",implode(",",error_get_last())."|81|\n", FILE_APPEND);				
                     if ($timeHis > 0) {
-file_put_contents("test.log",implode(",",error_get_last())."82\n", FILE_APPEND);			    
+file_put_contents("test.log",implode(",",error_get_last())."|82|\n", FILE_APPEND);			    
                         //передическая запись в бд
                         $link = connectDb(); writeDbArray(2, $link, $arrayResult, 'facebook'.tableName($userURL), $shiftArray); 
-file_put_contents("test.log",implode(",",error_get_last())."9\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|9|\n", FILE_APPEND);
 			//createTxt('test', mysqli_error($link));    
 			//echo mysqli_error($link);    
 			//mysqli_close($link);
@@ -126,7 +126,7 @@ file_put_contents("test.log",implode(",",error_get_last())."9\n", FILE_APPEND);
         }
     }
     $link = connectDb(); writeDbArray(2, $link, $arrayResult, 'facebook'.tableName($userURL), $shiftArray); 
-file_put_contents("test.log",implode(",",error_get_last())."10\n", FILE_APPEND);
+file_put_contents("test.log",implode(",",error_get_last())."|10|\n", FILE_APPEND);
 	//echo mysqli_error($link);   
 	//createTxt('test', mysqli_error($link));  
 	//mysqli_close($link);
