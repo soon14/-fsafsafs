2c05543e1e5e9aeb85e7a11840c4c359cea93ef9
MaksiWolf <36965057+MaksiWolf@users.noreply.github.com>
2019-03-01 14:25:21 +0300

    Update write_db.php

--- write_db.php
+++ write_db.php
@@ -2,11 +2,7 @@
 include 'dbConfiguration.php';
 function connectDb()
 {
-    $link = @mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
-    or die("Error: " . mysqli_error($link));
-    if (!mysqli_set_charset($link, "utf8")) {
-        printf("Error: " . mysqli_error($link));
-    }
+    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
     return $link;
 }
 function sqlHelper($nRequest, $element, $tableName)
